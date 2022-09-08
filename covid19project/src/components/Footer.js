11a9bda9802ecde48d0876d7bd991bd65a4086af
CharbelBG <charbel.boughazale@gmail.com>
2022-09-08 19:02:37 +0300

    Added the footer section

--- covid19project/src/components/Footer.js
+++ covid19project/src/components/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import styles from '../../styles/footer.module.css';
 export default function Footer(){
+    //devided into 3 parts 
     return(
+    <div className={styles.footerWrapper}>  
+      
         <div className={styles.footer}>
-            footer
+           <div><img src='/Logo.svg' /></div>
+           <div className={styles.links}>
+                <span>Overview</span>
+                <span>Symptoms</span>
+                <span>Prevention</span>
+                <span>Treatment</span>
+           </div>
+           <div className={styles.social}>
+            <img src="/f.svg" />
+            <img src="/youtube.png" />
+            <img src="/v.svg" />
+            <img src="/twitter.svg" />
+           </div>
         </div>
+           <p>2020 @ All right reseved by pixelspark.co</p>
+    </div>
     )
 } 
